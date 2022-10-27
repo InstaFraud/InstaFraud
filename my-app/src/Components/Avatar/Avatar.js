8f76fe692b0d2a215889669fe8097a9f324a2ea7
YaredBG <Yared_Gari@Mckinsey.com>
2022-10-27 11:56:34 -0400

    Added post truncating and like btn functionality

--- my-app/src/Components/Avatar/Avatar.js
+++ my-app/src/Components/Avatar/Avatar.js
@@ -2,10 +2,11 @@ import React from 'react'
 import './AvatarStyle.css'
 
 function Avatar({name, imageUrl, className=''}) {
+ 
   return (
     <div className='author'>
-    <img src='https://media.istockphoto.com/photos/beautiful-successful-latin-woman-smiling-picture-id1369508766?b=1&k=20&m=1369508766&s=170667a&w=0&h=xr3pk8VTmDoC9JXzEqMPL_4jZLiyIJWUMzKrBlVQiPI=' alt="pict" />
-    <p className={`post-text ${className}`}>Elsa Barbara</p>
+    <img src={`${imageUrl ||'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbX0lZHrfSwrUBz8gcmZ1mlUEXB8ULTGoJll6sEtiQ4VUiX-LwYUkOl9lv3MqgmRquack&usqp=CAU'} `} alt="user picture" />
+    <p className={`post-text ${className}`}>{name || 'User'}</p>
   </div>
   )
 }
